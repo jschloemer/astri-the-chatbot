fc26a08a5e4805dcf840450e3295d08c1b379a38
jschloemer <121447745+jschloemer@users.noreply.github.com>
2022-12-29 00:14:58 -0500

    Try again

--- actions/actions.py
+++ actions/actions.py
@@ -30,6 +30,7 @@ if (key is None):
 else:    
     openai.api_key = key
     useopenai = True
+useopenai = False
 
 class ActionSendAIGen(Action):
     
@@ -40,11 +41,11 @@ class ActionSendAIGen(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        if (useopenai):
+        if (useopenai and prompt is not None):
             prompt = tracker.get_slot("prompt")
             print(prompt)
             init="I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with Unknown.\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: "
-            fin = "\nA:"
+            fin = "?\nA:"
             dispatcher.utter_message(text="Looking up this question")
             
             total = init + prompt + fin
@@ -84,24 +85,34 @@ class ActionResetSearchSlot(Action):
     def name(self) -> Text:
         return "action_reset_search_slot"
     
-    def run(self, dispatcher, tracker, domain):
-        return [SlotSet("query", None), SlotSet("project", None)]
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        tracker.update_slot("query", None)
+        tracker.update_slot("project", None)
+        return []
 
 class ActionResetPartSlot(Action):
     
     def name(self) -> Text:
         return "action_reset_part_slot"
     
-    def run(self, dispatcher, tracker, domain):
-        return [SlotSet("part", None)]
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        tracker.update_slot("part", None)
+        return []
     
 class ActionResetPromptSlot(Action):
     
     def name(self) -> Text:
         return "action_reset_prompt_slot"
     
-    def run(self, dispatcher, tracker, domain):
-        return [SlotSet("prompt", None)]
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        tracker.update_slot("prompt", None)
+        return []
         
 class ActionPerformSearch(Action):
 
