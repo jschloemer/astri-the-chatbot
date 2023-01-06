67bfe3b24ebb02956d9445e5691b1fa0614c1490
jschloemer <121447745+jschloemer@users.noreply.github.com>
2023-01-05 16:50:42 -0800

    script to create additional training data

---
+++ dev-tools/openaiCat.py
@@ -0,0 +1,71 @@
+# Created as an example to try catagorization via openai
+# Author: Jeff Schloemer
+# Date: 01/04/2023
+
+# First, we need to install the elasticsearch and requests libraries:
+#!pip install requests openai bs4
+
+import openai
+from bs4 import BeautifulSoup
+import requests
+import argparse
+import os
+import re
+
+# Use the argparse library to parse the command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('url', help='The URL with data to catagorize')
+args = parser.parse_args()
+url = args.url
+
+# Setup OPENAI access
+key = os.getenv("OPENAI_API_KEY")
+useopenai = ""
+if (key is None):
+    useopenai = False
+    print("No OPENAI API Key Found - All external queries will be stopped")
+    exit()
+else:    
+    openai.api_key = key
+    useopenai = True
+    
+if (useopenai):
+    # Retrieve the HTML from the URL
+    r = requests.get(url)
+    html = r.text
+    
+    # Parse the HTML
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    # Extract the text content
+    tags = soup.find_all(['p', 'h1', 'li', 'h2', 'figcaption'])
+    text = ""
+    for tag in tags:
+        text = text + "\t\t" + tag.get_text()
+    text2 = soup.get_text()
+    text2 = str(text)
+    text2 = re.sub(r'\s\s+', '. ', text2)
+    print(text2)
+    print("=============================")
+    #text = text[:250]
+    
+    init="Extract multi-word adjective and noun phrases from text and return them in a comma seperated list. Do not include single words\nPrompt: The design concept that followed is based at the commercial available CubeSat structures; an aluminum frame consisting of four rails and four square parts surrounded by four sides.\nA: design concept, commercial available CubeSat structures, alumninum frame, four rails, four square parts, four sides\n\nPrompt: The basic reason for following this road is the mass saving and secondly the very good mechanical properties the Composite Materials offer. Finding the best lamination for the Composite sides the structure can withstand the applied loads at all three axes as verified via Finite Element Analysis.  \nA: basic reason, mass saving, very good mechanical properties, Composite Materials, best lamination, Composite sides, applied loads, three axes, Finite Element Analysis \n\nPrompt: "
+    prompt=text2
+    fin = "\nA:"
+    total = init + prompt + fin
+    print(total)
+    ops = True
+    
+    if (ops):
+        response = openai.Completion.create(
+                    model="text-davinci-003",
+                    prompt=total,
+                    temperature=0,
+                    max_tokens=100,
+                    top_p=1,
+                    frequency_penalty=0.0,
+                    presence_penalty=0.0,
+                    stop=["\n"]
+                )
+        print(response)
+        print(response['choices'][0]['text'])
