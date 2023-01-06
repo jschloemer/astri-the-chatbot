4c45ea2acdf66f7a8c44210882a21b7b73563636
jschloemer <121447745+jschloemer@users.noreply.github.com>
2023-01-05 17:10:07 -0800

    Update openaiCat to use cheaper model

--- dev-tools/openaiCat.py
+++ dev-tools/openaiCat.py
@@ -38,34 +38,36 @@ if (useopenai):
     soup = BeautifulSoup(html, 'html.parser')
     
     # Extract the text content
-    tags = soup.find_all(['p', 'h1', 'li', 'h2', 'figcaption'])
-    text = ""
-    for tag in tags:
-        text = text + "\t\t" + tag.get_text()
     text2 = soup.get_text()
-    text2 = str(text)
+    text2 = str(text2)
     text2 = re.sub(r'\s\s+', '. ', text2)
-    print(text2)
-    print("=============================")
-    #text = text[:250]
+    #print(text2)
+    #print("=============================")
     
     init="Extract multi-word adjective and noun phrases from text and return them in a comma seperated list. Do not include single words\nPrompt: The design concept that followed is based at the commercial available CubeSat structures; an aluminum frame consisting of four rails and four square parts surrounded by four sides.\nA: design concept, commercial available CubeSat structures, alumninum frame, four rails, four square parts, four sides\n\nPrompt: The basic reason for following this road is the mass saving and secondly the very good mechanical properties the Composite Materials offer. Finding the best lamination for the Composite sides the structure can withstand the applied loads at all three axes as verified via Finite Element Analysis.  \nA: basic reason, mass saving, very good mechanical properties, Composite Materials, best lamination, Composite sides, applied loads, three axes, Finite Element Analysis \n\nPrompt: "
     prompt=text2
     fin = "\nA:"
     total = init + prompt + fin
-    print(total)
+    #print(total)
     ops = True
     
     if (ops):
         response = openai.Completion.create(
-                    model="text-davinci-003",
+                    model="text-babbage-001",
                     prompt=total,
                     temperature=0,
-                    max_tokens=100,
+                    max_tokens=300,
                     top_p=1,
                     frequency_penalty=0.0,
                     presence_penalty=0.0,
                     stop=["\n"]
                 )
         print(response)
-        print(response['choices'][0]['text'])
+        #print(response['choices'][0]['text'])
+        output = response['choices'][0]['text']
+        
+        stringList = output.split(',')
+        for line in stringList:
+            print('- ' + line)
+        
+        
